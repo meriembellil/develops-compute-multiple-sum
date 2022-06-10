e0edf2f737c2a191e859b23b3d05909b66b0e5a7
meriembellil <90050112+meriembellil@users.noreply.github.com>
2022-06-10 20:18:18 +0100

    create function calcul sum p

--- index.php
+++ index.php
@@ -1,3 +1,21 @@
 <?php
-echo(abs(-6.7));  // returns 6.7
+
+$n=11;
+
+echo compte_multiple_sump($n);
+
+function compte_multiple_sump($n)
+{
+    $result = 0;
+    if($n>0 && $n<1000){
+        
+        for ($i=0; $i<$n; $i++) {
+            if (($i%3 ===0 )||($i%5 ===0) || ($i%7 ===0)){
+                $result = $result + $i;
+            }   
+        }
+    }
+   
+    return $result;
+}
 ?>
